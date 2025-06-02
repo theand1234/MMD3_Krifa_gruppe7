ed1ced197d7b12cc36912ab32818b91ff7e2db77
AngeKri <ange0437.k@gmail.com>
2025-06-02 20:48:55 +0200

    test

--- src/Pages/IndexPage.vue
+++ src/Pages/IndexPage.vue
@@ -4,9 +4,17 @@ import reviewInfo from "../assets/data/trustpilot.json"
 </script>
 
 <template>
-
+<div>
+    <p>
+        hello
+    </p>
+</div>
 </template>
 
 <style scoped>
-  
+  div{
+    height: 3rem;
+    width: auto;
+    background-color: red;
+  }
 </style>
