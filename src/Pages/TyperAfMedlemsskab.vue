9002d216990ed8143bb8e1ca1dbbb90cf4f76dfd
theand1234 <andreas147895623@gmail.com>
2025-06-03 19:53:15 +0200

    s

--- src/Pages/TyperAfMedlemsskab.vue
+++ src/Pages/TyperAfMedlemsskab.vue
@@ -4,11 +4,63 @@
 
 <template>
     <main>
-<RouterLink to="/">homepage</RouterLink>
-<p>kan mann se det her</p>
+<section><div><h2>A-kasse</h2><p>Få op til 24.199 kr. om måneden i dagpenge, hjælp til at komme videre i din karriere og adgang til attraktive rabatter.
+<ul><li>Dagpenge</li>
+<li>Mulighed for Lønsikring</li>
+<li>Professionel karriererådgivning</li>
+<li>Personlig jobkonsulent</li></ul></p><button>LÆS MERE OM A-KASSE</button></div><img src="../assets/Img/A-kasse.png" alt=""></section>
+<section><div><h2>Fagforening</h2><p>Vi kender dine rettigheder, når du ikke selv gør.
+<ul><li>Juridisk rådgivning</li>
+<li>Forhandling med arbejdsgivere</li>
+<li>Sagføring i retten</li>
+<li>Personlig karrierekonsulen</li></ul>
+</p><button>LÆS MERE OM FAGFORENING</button></div><img src="../assets/Img/Fagforening.png" alt=""></section>
+<section><div><h2>Under uddannelse</h2><p>Gratis fagforening og a-kasse for dig under uddannelse.
+<ul><li>Gratis a-kasse</li>
+<li>Personlig karrierekonsulent</li>
+<li>Juridisk rådgivning</li>
+<li>Gratis psykologhjælp</li></ul>
+</p><button>LÆS MERE OM GRATIS MEDLEMSKAB</button></div><img src="../assets/Img/" alt=""></section>
+<section><div><h2>Krifa Selvstændige</h2><p>Masser af fordele til dig, der er selvstændig på fuldtid.
+<ul><li>Juridisk rådgivning</li>
+<li>Pensionsordning gennem Danica Pension</li>
+<li>Rabat på forsikringer, brændstof m.m.</li>
+<li>Hjælp til stress og trivsel</li></ul>
+</p><button>LÆS MERE OM KRIFA SELVSTÆNDIGE</button></div><img src="" alt=""></section>
 </main>
 </template>
 
 <style scoped>
+section{
+    display: flex;
+    padding: 2rem 5rem;
+    gap: 5rem;
+}
+li{
+    list-style: circle;
+}
+button{
+    font-size: 20px;
+    font-weight: 700;
+    border-radius: 46px;
+    background-color: #F1e13d;
+    border: none;
+    width: 303px;
+    height: 46px;
+    align-self: center;
+}
+img{
+object-fit: cover;
+width: 35%;
+
+}
+p{
+width: 75ch;
+}
+div{
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+}
 
 </style>
