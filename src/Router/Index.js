46267c57b912b764e5d87d6293bfe3eaa1d4c1ad
theand1234 <andreas147895623@gmail.com>
2025-06-04 19:04:53 +0200

    s

--- src/Router/Index.js
+++ src/Router/Index.js
@@ -3,6 +3,8 @@ import IndexPage from '../Pages/IndexPage.vue'
 import TyperAfMedlemsskab from '../Pages/typer-af-Medlemsskab.vue';
 import Login from '../Pages/Login.vue';
 import Mitkrifa from '../Pages/Mitkrifa.vue';
+import Blivmedlem from '../Pages/Blivmedlem.vue';
+import MedlemSomStuderende from '../Pages/MedlemSomStuderende.vue'
 
 const router = createRouter({
     history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -26,7 +28,18 @@ const router = createRouter({
             path: '/MitKrifa',
             name: 'Mitkrifa',
             component: Mitkrifa
+        },
+        {
+            path: '/Bliv-Medlem',
+            name: 'BlivMedlem',
+            component: Blivmedlem
+        },
+        {
+            path: '/Medlem-som-studerende',
+            name: 'MedlemSomStuderende',
+            component: MedlemSomStuderende
         }
+
 
 
 
