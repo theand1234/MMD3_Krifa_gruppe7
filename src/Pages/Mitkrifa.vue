8e095a50bdcd28cb63c276d81657906b5c15579f
AngeKri <ange0437.k@gmail.com>
2025-06-05 08:30:04 +0200

    Update Mitkrifa.vue

--- src/Pages/Mitkrifa.vue
+++ src/Pages/Mitkrifa.vue
@@ -1,5 +1,25 @@
 <script setup>
 
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  const accordionItemHeaders = document.querySelectorAll(".accordionItemHeader")
+//On click, give each item class "active"
+  accordionItemHeaders.forEach((header) => {
+    header.addEventListener("click", () => {
+      header.classList.toggle("active")
+
+//If class "active" present, expand
+      const accordionItemBody = header.nextElementSibling
+      if (header.classList.contains("active")) {
+        accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px"
+      } else {
+        accordionItemBody.style.maxHeight = "0"
+      }
+    })
+  })
+})
+
 </script>
 
 <template>
@@ -16,7 +36,7 @@
         <a href="#" class="FEP">
             <div>
                 <i class="fa-solid fa-inbox"></i>
-                <h2>Krifa Boks</h2>
+                <h3>Krifa Boks</h3>
             </div>
             <div>
                 <p>Se dine dokumenter og breve</p>
@@ -26,7 +46,7 @@
         <a href="#" class="FEP">
             <div>
                 <i class="fa-solid fa-id-card"></i>
-                <h2>Meld dig ledig</h2>
+                <h3>Meld dig ledig</h3>
             </div>
             <div>
                 <p>Følg vores tre-trins guide.</p>
@@ -36,7 +56,7 @@
         <a href="#" class="FEP">
             <div>
                 <i class="fa-solid fa-calendar"></i>
-                <h2>Book samtale</h2>   
+                <h3>Book samtale</h3>   
             </div>
             <div>
                 <p>Se vores udvalg af samtaler, der kan hjælpe dig i dit arbejdsliv</p>
@@ -44,13 +64,62 @@
             </div>           
         </a>
     </section>
-    <section class="accordion-container">
-        <h2>Selvbetjening</h2>
-        <p>Udfyld blanketter, book samtaler og kurser, betalt kontigent, meld dig ledig mv.</p>
-        <div class="accordion-container">
-           
-                
+    <section class="accordionContainer">     
+        <div class="accordionHead">
+            <h2>Selvbetjening</h2>
+            <p>Udfyld blanketter, book samtaler og kurser, betalt kontigent, meld dig ledig mv.</p>
         </div>
+        <div class="accordion">
+            <div>
+                <div class="accordionItem">
+                    <div class="accordionItemHeader">Ledig<i class="fa-solid fa-angle-down"></i></div>
+                    <div class="accordionItemBody">
+                        <a class="accordionItemContent" href="#">Meld dig ledig</a>
+                        <a class="accordionItemContent" href="#">Joblog</a>
+                        <a class="accordionItemContent" href="#">Sidste dag som ledig</a>
+                    </div>
+                </div>       
+                <div class="accordionItem">
+                    <div class="accordionItemHeader">Lønsikring<i class="fa-solid fa-angle-down"></i></div>
+                    <div class="accordionItemBody">
+                        <a class="accordionItemContent" href="#">Er du opsagt?</a>
+                        <a class="accordionItemContent" href="#">Lønsikringsberegner</a>
+                        <a class="accordionItemContent" href="#">Spørgsmål og svar</a>
+                    </div>
+                </div>       
+                <div class="accordionItem">
+                    <div class="accordionItemHeader">Feriedagpenge<i class="fa-solid fa-angle-down"></i></div>
+                    <div class="accordionItemBody">
+                        <a class="accordionItemContent" href="#">Søg om feriedagpenge</a>
+                    </div>
+                </div>       
+            </div>
+            <div>
+                <div class="accordionItem">
+                    <div class="accordionItemHeader">Blanketter<i class="fa-solid fa-angle-down"></i></div>
+                    <div class="accordionItemBody">
+                        <a class="accordionItemContent" href="#">Find blanketter</a>
+                        <a class="accordionItemContent" href="#">Indsendte blanketter</a>
+                    </div>
+                </div>       
+                <div class="accordionItem">
+                    <div class="accordionItemHeader">Samtaler og Jobsøgning<i class="fa-solid fa-angle-down"></i></div>
+                    <div class="accordionItemBody">
+                        <a class="accordionItemContent" href="#">Book samtale</a>
+                        <a class="accordionItemContent" href="#">Job- og KarrierePortal</a>
+                    </div>
+                </div>       
+                <div class="accordionItem">
+                    <div class="accordionItemHeader">Medlemskab<i class="fa-solid fa-angle-down"></i></div>
+                    <div class="accordionItemBody">
+                        <a class="accordionItemContent" href="#">Kontingent</a>
+                        <a class="accordionItemContent" href="#">Deltids- eller fuldstidsmedlem</a>
+                        <a class="accordionItemContent" href="#">Meld din ven ind</a>
+                        <a class="accordionItemContent" href="#">Meld dig ud</a>
+                    </div>
+                </div>       
+            </div>
+        </div>    
     </section>
 </template>
 
@@ -60,6 +129,67 @@
 /* dropdown menu */
 /* --------------------------------------------------------------------------------------------------------- */
 
+.accordionContainer {
+    width: 100%;
+    max-width: 80rem;
+    margin: 0 auto;
+    padding: 4rem 3rem;
+    box-shadow: 0px 5px 14px 0px #0000003d;
+}
+.accordionHead{
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+}
+
+.accordionHead{
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+}
+
+.accordionHead p{
+    font-weight: 300;
+}
+
+.accordion{
+    display: flex;
+    justify-content: space-around;
+}
+
+.accordionItem{
+    width: 35rem;
+}
+
+.accordionItemHeader {
+    border-bottom: 4px solid var(--Krifa-Lilla);
+    padding-left: 1rem;
+    font-size: 1.2rem;
+    font-weight: 700;
+    font-style: italic;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+.accordionItemHeader i{
+    transition: transform 0.5s ease;
+    padding: 1rem;
+}
+.accordionItemHeader.active i{
+    transform: rotate(180deg);
+}
+.accordionItemBody {
+    display: flex;
+    flex-direction: column;
+    max-height: 0;
+    overflow: hidden;
+    transition: max-height 0.4s ease-out;
+}
+.accordionItemContent {
+    padding: 1rem; 
+    text-decoration: underline;
+    font-weight: 600;
+}
 
 /* --------------------------------------------------------------------------------------------------------- */
 /* Hero */
@@ -89,9 +219,10 @@ h1{
 /* FEP */
 /* --------------------------------------------------------------------------------------------------------- */
 
-h2{
+h3{
     font-size: 2rem;
     font-style: normal;
+    font-weight: 900;
 }
 .FEPs{
     display: flex;
