124550e2aa58b9738f4266ba0736a3efc4332975
AngeKri <ange0437.k@gmail.com>
2025-06-05 09:30:37 +0200

    Accordion mobile toggle

--- src/Pages/Mitkrifa.vue
+++ src/Pages/Mitkrifa.vue
@@ -1,19 +1,35 @@
 <script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
 
-import { onMounted } from 'vue'
+const isMobile = ref(window.innerWidth < 1000)
+const bigAccordionOpen = ref(false)
+
+const onResize = () => {
+//check if window size is above 1000px. if yes, keep bigAccordion open
+  isMobile.value = window.innerWidth < 1000
+  if (!isMobile.value) bigAccordionOpen.value = true
+}
+
+onUnmounted(() => {
+  window.removeEventListener('resize', onResize)
+})
 
 onMounted(() => {
-  const accordionItemHeaders = document.querySelectorAll(".accordionItemHeader")
+    onResize()
+    window.addEventListener('resize', onResize)
+
+    const accordionItemHeaders = document.querySelectorAll(".accordionItemHeader")
 //On click, give each item class "active"
-  accordionItemHeaders.forEach((header) => {
-    header.addEventListener("click", () => {
-      header.classList.toggle("active")
+     accordionItemHeaders.forEach((header) => {
+        header.addEventListener("click", () => {
+        header.classList.toggle("active")
 
 //If class "active" present, expand
-      const accordionItemBody = header.nextElementSibling
-      if (header.classList.contains("active")) {
-        accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px"
-      } else {
+    const accordionItemBody = header.nextElementSibling
+        if (header.classList.contains("active")) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px"
+        } 
+        else {
         accordionItemBody.style.maxHeight = "0"
       }
     })
@@ -21,7 +37,6 @@ onMounted(() => {
 })
 
 </script>
-
 <template>
     <section class="hero">
         <svg class="shape-left" xmlns="http://www.w3.org/2000/svg" width="387" height="270" viewBox="0 0 387 270" fill="none">
@@ -64,82 +79,116 @@ onMounted(() => {
             </div>           
         </a>
     </section>
-    <section class="accordionContainer">     
+    <section class="accordionContainer">
         <div class="accordionHead">
             <h2>Selvbetjening</h2>
-            <p>Udfyld blanketter, book samtaler og kurser, betalt kontigent, meld dig ledig mv.</p>
+            <p>Udfyld blanketter, book samtaler og kurser, betalt kontingent, meld dig ledig mv.</p>
         </div>
-        <div class="accordion">
+        <div v-if="isMobile" class="bigAccordionToggle" @click="bigAccordionOpen = !bigAccordionOpen">
+            <div class="bigAccordionHeader">
+                <div class="accordionHeadMobile">
+                    <h2>Selvbetjening</h2>
+                    <p>Udfyld blanketter, book samtaler og kurser, betalt kontingent, meld dig ledig mv.</p>
+                </div>
+                <i class="fa-solid fa-angle-down"></i>
+            </div>
+        </div>
+        <!--Add class "open" if bigAccordionOpen is true or not on Mobile  -->
+        <div class="accordion" :class="{ open: bigAccordionOpen || !isMobile }">
             <div>
                 <div class="accordionItem">
-                    <div class="accordionItemHeader">Ledig<i class="fa-solid fa-angle-down"></i></div>
+                    <div class="accordionItemHeader">
+                        Ledig <i class="fa-solid fa-angle-down"></i>
+                    </div>
                     <div class="accordionItemBody">
                         <a class="accordionItemContent" href="#">Meld dig ledig</a>
                         <a class="accordionItemContent" href="#">Joblog</a>
                         <a class="accordionItemContent" href="#">Sidste dag som ledig</a>
                     </div>
-                </div>       
+                </div>
                 <div class="accordionItem">
-                    <div class="accordionItemHeader">Lønsikring<i class="fa-solid fa-angle-down"></i></div>
+                    <div class="accordionItemHeader">
+                        Lønsikring <i class="fa-solid fa-angle-down"></i>
+                    </div>
                     <div class="accordionItemBody">
                         <a class="accordionItemContent" href="#">Er du opsagt?</a>
                         <a class="accordionItemContent" href="#">Lønsikringsberegner</a>
                         <a class="accordionItemContent" href="#">Spørgsmål og svar</a>
                     </div>
-                </div>       
+                </div>
                 <div class="accordionItem">
-                    <div class="accordionItemHeader">Feriedagpenge<i class="fa-solid fa-angle-down"></i></div>
+                    <div class="accordionItemHeader">
+                        Feriedagpenge <i class="fa-solid fa-angle-down"></i>
+                    </div>
                     <div class="accordionItemBody">
                         <a class="accordionItemContent" href="#">Søg om feriedagpenge</a>
                     </div>
-                </div>       
+                </div>
             </div>
             <div>
                 <div class="accordionItem">
-                    <div class="accordionItemHeader">Blanketter<i class="fa-solid fa-angle-down"></i></div>
+                    <div class="accordionItemHeader">
+                        Blanketter <i class="fa-solid fa-angle-down"></i>
+                    </div>
                     <div class="accordionItemBody">
                         <a class="accordionItemContent" href="#">Find blanketter</a>
                         <a class="accordionItemContent" href="#">Indsendte blanketter</a>
                     </div>
-                </div>       
+                </div>
                 <div class="accordionItem">
-                    <div class="accordionItemHeader">Samtaler og Jobsøgning<i class="fa-solid fa-angle-down"></i></div>
+                    <div class="accordionItemHeader">
+                        Samtaler og Jobsøgning <i class="fa-solid fa-angle-down"></i>
+                    </div>
                     <div class="accordionItemBody">
                         <a class="accordionItemContent" href="#">Book samtale</a>
                         <a class="accordionItemContent" href="#">Job- og KarrierePortal</a>
                     </div>
-                </div>       
+                </div>
                 <div class="accordionItem">
-                    <div class="accordionItemHeader">Medlemskab<i class="fa-solid fa-angle-down"></i></div>
+                    <div class="accordionItemHeader">
+                        Medlemskab <i class="fa-solid fa-angle-down"></i>
+                    </div>
                     <div class="accordionItemBody">
                         <a class="accordionItemContent" href="#">Kontingent</a>
                         <a class="accordionItemContent" href="#">Deltids- eller fuldstidsmedlem</a>
                         <a class="accordionItemContent" href="#">Meld din ven ind</a>
                         <a class="accordionItemContent" href="#">Meld dig ud</a>
                     </div>
-                </div>       
+                </div>
             </div>
-        </div>    
-    </section>
+        </div>
+  </section>
+    <section class="accordionContainer">
+        <div class="accordionHead">
+            <h2>Mine oplysninger</h2>
+            <p>Ret dine oplysninger og se dine køb</p>
+        </div>
+        <div v-if="isMobile" class="bigAccordionToggle" @click="bigAccordionOpen = !bigAccordionOpen">
+            <div class="bigAccordionHeader">
+                <div class="accordionHeadMobile">
+                    <h2>Selvbetjening</h2>
+                    <p>Udfyld blanketter, book samtaler og kurser, betalt kontingent, meld dig ledig mv.</p>
+                </div>
+                <i class="fa-solid fa-angle-down"></i>
+            </div>
+        </div>
+        <!--Add class "open" if bigAccordionOpen is true or not on Mobile  -->
+        <div class="accordion" :class="{ open: bigAccordionOpen || !isMobile }">
+            
+        </div>
+  </section>
 </template>
 
 <style scoped>
-
 /* --------------------------------------------------------------------------------------------------------- */
 /* dropdown menu */
 /* --------------------------------------------------------------------------------------------------------- */
-
 .accordionContainer {
     width: 100%;
     max-width: 80rem;
     margin: 0 auto;
     padding: 4rem 3rem;
     box-shadow: 0px 5px 14px 0px #0000003d;
-}
-.accordionHead{
-    display: flex;
-    align-items: center;
-    gap: 1rem;
 }
 
 .accordionHead{
@@ -148,16 +197,20 @@ onMounted(() => {
     gap: 2rem;
 }
 
-.accordionHead p{
+.accordionHead p {
     font-weight: 300;
 }
 
-.accordion{
+.accordion {
     display: flex;
     justify-content: space-around;
+    gap: 2rem;
+    flex-wrap: wrap;
+    overflow: hidden;
+    transition: max-height 0.4s ease;
 }
 
-.accordionItem{
+.accordionItem {
     width: 35rem;
 }
 
@@ -171,13 +224,16 @@ onMounted(() => {
     align-items: center;
     justify-content: space-between;
 }
-.accordionItemHeader i{
+
+.accordionItemHeader i {
     transition: transform 0.5s ease;
     padding: 1rem;
 }
+
 .accordionItemHeader.active i{
     transform: rotate(180deg);
 }
+
 .accordionItemBody {
     display: flex;
     flex-direction: column;
@@ -185,12 +241,51 @@ onMounted(() => {
     overflow: hidden;
     transition: max-height 0.4s ease-out;
 }
+
 .accordionItemContent {
-    padding: 1rem; 
+    padding: 1rem;
     text-decoration: underline;
     font-weight: 600;
 }
 
+.bigAccordionToggle {
+    display: none;
+    margin-bottom: 1rem;
+}
+
+.bigAccordionHeader {
+    font-size: 1.4rem;
+    font-weight: bold;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 0;
+}
+
+
+
+@media (max-width: 1000px) {
+    .accordionHead{
+        display: none;
+    }
+
+    .bigAccordionToggle {
+        display: block;
+    }
+
+    .accordion {
+        max-height: 0;
+        flex-direction: column;
+    }
+
+    .accordion.open {
+        max-height: 3000px; 
+    }
+
+    .accordionItem {
+        width: 100%;
+  }
+}
 /* --------------------------------------------------------------------------------------------------------- */
 /* Hero */
 /* --------------------------------------------------------------------------------------------------------- */
@@ -253,6 +348,10 @@ i{
     font-size: 1.5rem;
     padding: 1rem 1rem 1rem 0;
 }
+
+/* --------------------------------------------------------------------------------------------------------- */
+/* Media Queries */
+/* --------------------------------------------------------------------------------------------------------- */
 
 
 @media (max-width: 600px) {
